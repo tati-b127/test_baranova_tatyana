0aea9d60c75f3fd024747a556f58543ebeb91211
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2023-06-28 13:26:57 +0300

    styles complite, background added, select options in index.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,19 @@ const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
 const browsersync = require("browser-sync").create();
+const babel = require("gulp-babel");
+const notify = require("gulp-notify");
+const uglify = require("gulp-uglify-es").default;
+const image = require("gulp-image");
 
-// const del = require("del");
+const del = require("del");
 
 const fonts = () => {
 	return src("src/fonts/**").pipe(dest("dist/fonts/"));
 };
-// const clean = () => {
-// 	return del(["dist/"]);
-// };
+const clean = () => {
+	return del(["dist/"]);
+};
 const html = () => {
 	return src("src/**/*.html")
 		.pipe(htmlMin({ collapseWhitespace: true }))
@@ -60,30 +64,42 @@ const stylesDev = () => {
 			.pipe(browsersync.stream())
 	);
 };
-// const scripts = () => {
-// 	return src(["src/resources/**/*.js", "src/js/**/*.js"])
-// 		.pipe(sourcemaps.init())
-// 		.pipe(
-// 			babel({
-// 				presets: ["@babel/preset-env"],
-// 			})
-// 		)
-// 		.pipe(concat("app.js"))
-// 		.pipe(
-// 			uglify({
-// 				toplevel: true, //убирает все переменные
-// 			}).on("error", notify.onError())
-// 		)
-// 		.pipe(sourcemaps.write())
-// 		.pipe(dest("dist/"))
-// 		.pipe(browsersync.stream());
-// };
-// const scriptsDev = () => {
-// 	return src(["src/resources/**/*.js", "src/js/**/*.js"])
-// 		.pipe(concat("app.js"))
-// 		.pipe(dest("dist/"))
-// 		.pipe(browsersync.stream());
-// };
+const scripts = () => {
+	return src(["src/script/**/*.js"])
+		.pipe(sourcemaps.init())
+		.pipe(
+			babel({
+				presets: ["@babel/preset-env"],
+			})
+		)
+		.pipe(concat("app.js"))
+		.pipe(
+			uglify({
+				toplevel: true, //убирает все переменные
+			}).on("error", notify.onError())
+		)
+		.pipe(sourcemaps.write())
+		.pipe(dest("dist/"))
+		.pipe(browsersync.stream());
+};
+const scriptsDev = () => {
+	return src(["src/script/**/*.js"])
+		.pipe(concat("app.js"))
+		.pipe(dest("dist/"))
+		.pipe(browsersync.stream());
+};
+const images = () => {
+	return src([
+		"src/img/**/*.jpg",
+		"src/img/**/*.png",
+		"src/img/**/*.jpeg",
+		"src/img/**/*.svg",
+		"src/img/**/*.webp",
+		"src/img/**/*.ico",
+	])
+		.pipe(image())
+		.pipe(dest("dist/images/"));
+};
 
 const watchFiles = () => {
 	browsersync.init({
@@ -96,9 +112,24 @@ const watchFiles = () => {
 watch("src/fonts/**", fonts);
 watch("src/styles/**/*.scss", styles);
 watch("src/styles/**/*.css", styles);
-// watch("src/js/**/*.js", scripts);
-// watch("src/js/**/*.js", scriptsDev);
+watch("src/script/**/*.js", scripts);
+watch("src/script/**/*.js", scriptsDev);
 watch("src/*.html", html);
+watch("src/img/**/*.png", images);
+watch("src/img/**/*.jpg", images);
+watch("src/img/**/*.svg", images);
 
-exports.default = series(parallel(html, fonts), styles, watchFiles);
-exports.dev = series(parallel(html, fonts), stylesDev, watchFiles);
+exports.default = series(
+	clean,
+	parallel(html, fonts, images),
+	scripts,
+	styles,
+	watchFiles
+);
+exports.dev = series(
+	clean,
+	parallel(html, fonts, images),
+	scriptsDev,
+	stylesDev,
+	watchFiles
+);
